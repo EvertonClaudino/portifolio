da18d6b80bd7b18a1de30f37ee9172b654c62659
Everton Claudino <evertonclaudinoit@gmail.com>
2022-07-19 01:03:36 +0100

    funcionalidades
    add Resonsive, Menu button mobile

--- script.js
+++ script.js
@@ -179,3 +179,18 @@ textgit.classList.add("hide");
 
 textfix.classList.remove("hide");
 });
+
+
+
+const btnMobile = document.getElementById('btn-mobile')
+
+function toggleMenu(event ){
+    if(event.type === 'touchstart') event.preventDefault();
+    const nav = document.getElementById('navbar')
+    nav.classList.toggle('active')
+    const active = nav.classList.contains('active');
+
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
